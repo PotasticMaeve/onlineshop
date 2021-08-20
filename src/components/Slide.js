1ee5c3c42009f1d69d8fb9cbcfe047b2f27cd2c4
Claudia Felicia <claudia@localhost.localdomain>
2021-08-20 13:38:31 +0700

    adding automatic slide banner

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
+const colors = ["#FFADAD", "#0088FE", "#00C49F", "#FFBB28"];
 const Slide = () => {
-  return <div className="slide-wrap"></div>;
+  const [index, setIndex] = useState(0);
+  const delay = 5000;
+
+  useEffect(() => {
+    setTimeout(
+      () =>
+        setIndex((prevIndex) =>
+          prevIndex === colors.length - 1 ? 0 : prevIndex + 1
+        ),
+      delay
+    );
+    return () => {};
+  }, [index]);
+  return (
+    <div className="slide-wrap">
+      <div
+        className="slider"
+        style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+      >
+        {colors.map((backgroundColor, idx) => (
+          <div key={idx} className="slide" style={{ backgroundColor }}></div>
+        ))}
+      </div>
+      <div className="slide-dots">
+        {colors.map((_, idx) => (
+          <div
+            key={idx}
+            className={`dot${index === idx ? " active" : ""} `}
+            onClick={() => {
+              setIndex(idx);
+            }}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Slide;
